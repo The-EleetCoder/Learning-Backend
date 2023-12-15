4f0ccd1ee7cda55028cfe8bb3a7b87efbc1d6629
The-EleetCoder <jaijain1803@gmail.com>
2023-12-15 12:37:21 +0530

    class 2

--- class2/config/database.js
+++ class2/config/database.js
@@ -1,5 +1,18 @@
 const mongoose = require('mongoose');
 
+require("dotenv").config();
+
 const dbConnect = () => {
-    mongoose.connect(process.env.DATABASE_URL);
+    mongoose.connect(process.env.DATABASE_URL,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    })
+    .then(()=>{
+        console.log("DB connection established");
+    })
+    .catch(()=>{
+        console.log("DB connection failed");
+        console.error(error.message);
+        process.exit(1);
+    })
 }
