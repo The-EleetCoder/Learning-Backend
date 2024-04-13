fa01bc11dc15de6f36dcbf8e7c5e0b269be5df4c
The-EleetCoder <jaijain1803@gmail.com>
2024-04-13 18:45:31 +0530

    class 5

--- class5/AuthApp/controllers/Auth.js
+++ class5/AuthApp/controllers/Auth.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 exports.signup = async (req, res) => {
   try {
@@ -11,7 +13,6 @@ exports.signup = async (req, res) => {
         message: "User already exists",
       });
     }
-
     // hashing password
     let hashedPassword;
     try {
@@ -22,7 +23,6 @@ exports.signup = async (req, res) => {
         message: "Error in hashing password",
       });
     }
-
     // save in database
     const user = await User.create({
       name,
@@ -30,7 +30,6 @@ exports.signup = async (req, res) => {
       password: hashedPassword,
       role,
     });
-
     return res.status(200).json({
       success: true,
       message: "User created successfully",
@@ -41,5 +40,66 @@ exports.signup = async (req, res) => {
       success: false,
       message: "User creation failed! Please try again later.",
     });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Please fill all the details",
+      });
+    }
+
+    const user = await User.findOne({ email }).lean();
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User not Registered",
+      });
+    }
+
+    const payload = {
+      email: user.email,
+      id: user._id,
+      password: user.password,
+    };
+
+    if (await bcrypt.compare(password, user.password)) {
+      // creating jwt
+      let token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "2hr",
+      });
+
+      user.token = token;
+      user.password = undefined;
+
+      const options = {
+        expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+      };
+
+      res.cookie("token", token, options).status(200).json({
+        success: true,
+        user,
+        token,
+        message: "user logged in successfully",
+      });
+    } else {
+      return res.status(403).json({
+        success: false,
+        message: "Password is incorrect",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      success: false,
+      message: "Login failure"
+    })
   }
 };
